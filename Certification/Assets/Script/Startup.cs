e0ceb48d2af258ca1c4115a5465e5fb604bbb1fa
Igor Henrique Santos <igor.hos@gmail.com>
2018-10-28 14:37:27 -0200

    First Commit

---
+++ Certification/Assets/Script/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Startup : MonoBehaviour {
+
+    public Button btPlay;
+
+	void Start () 
+    {
+        
+        btPlay.onClick.AddListener(PlayGame);
+	}
+	
+    private void PlayGame()
+    {
+        SceneManager.LoadScene("Persistent", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Level1", LoadSceneMode.Additive);
+   
+    }
+}
